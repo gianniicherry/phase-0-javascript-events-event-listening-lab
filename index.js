0f478d2c846aad1ff799595afea1911f017684b9
Gianni <gianniicherry@gmail.com>
2022-09-26 12:45:52 -0700

    commit tree bb975a000e008505ad2818f03d73d5693540b837

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById('button')
+    function clickAlert(){
+        alert('I was Clicked!')
+    }
+
+    input.addEventListener('click',clickAlert);
 }
+
